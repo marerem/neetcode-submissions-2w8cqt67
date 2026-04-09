57a09948c55c57c044d503a8f15eb7dba87c02b2
Mariia Eremina <101661237+marerem@users.noreply.github.com>
2026-04-09 15:44:24 +0200

    Add: anagram-groups - submission-18

---
+++ Data Structures & Algorithms/anagram-groups/submission-18.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> List[List[str]]:
+        store = defaultdict(list) # key anagram : value is list of thess words
+        for word in strs:
+            anagram = [0]*26
+            for w in word:
+                index = ord("a") - ord(w)
+                anagram[index] += 1
+            
+            store[tuple(anagram)].append(word)
+        return list(store.values())
+
+
+
