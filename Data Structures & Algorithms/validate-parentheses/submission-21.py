e2f92d8daa55340e5c45cb3dca8fa9fb8b82c1a8
Mariia Eremina <101661237+marerem@users.noreply.github.com>
2026-04-16 23:03:28 +0200

    Add: validate-parentheses - submission-21

---
+++ Data Structures & Algorithms/validate-parentheses/submission-21.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        map_ = {')':'(','}':'{',']':'['}
+        tmp = []# keep opens [[
+        for i in s:
+            if i not in map_: #open
+                tmp.append(i)
+                print(tmp)
+            elif i in map_ and tmp:
+                if map_[i] == tmp[-1]:
+                    tmp.pop(-1)
+                else:
+                    return False
+            else:
+                return False
+        return True if not tmp else False
+      
+       
